a9b8ddd29eea53dd3a0c0404423b7d4463b58cd5
jiseonn97 <jiseonn97@naver.com>
2023-09-08 18:17:48 +0900

    2023.09.08

--- dist/js/reservation.js
+++ dist/js/reservation.js
@@ -7,4 +7,10 @@ $(function () {
             $li.toggleClass("show");
         }
     });
+
+    flatpickr("#datepicker", {
+        dateFormat: "Y-m-d", // 날짜 형식 설정
+        inline: true, // 달력을 인라인 모드로 표시합니다.
+        minDate: "today", // 오늘 이전의 날짜를 선택할 수 없도록 설정
+    });
 });
